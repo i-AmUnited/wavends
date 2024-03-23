017e4751fe7ac4b33ea7ef6a89670523b584f225
Tobi <tobiamusa94@gmail.com>
2024-03-23 13:12:11 +0100

    Update customStyleConfig.js

--- Assets/js/customStyleConfig.js
+++ Assets/js/customStyleConfig.js
@@ -5,7 +5,8 @@ tailwind.config = {
           custom: '-32px 0px 48px -20px rgba(0, 0, 0, 0.3)',
         },
         colors: {
-          brand: '#33FFAD',
+          brand: '#008000',
+          brand_2: '#33FFAD',
         }
       }
     }
