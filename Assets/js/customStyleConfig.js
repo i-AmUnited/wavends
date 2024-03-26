87ec340c8c4d6937bc344064af72a8b74cdf100d
Tobi <tobiamusa94@gmail.com>
2024-03-26 15:51:39 +0000

    Add files via upload

--- Assets/js/customStyleConfig.js
+++ Assets/js/customStyleConfig.js
@@ -5,8 +5,7 @@ tailwind.config = {
           custom: '-32px 0px 48px -20px rgba(0, 0, 0, 0.3)',
         },
         colors: {
-          brand: '#138808',
-          brand_2: '#33FFAD',
+          brand: '#3aafa9',
         }
       }
     }
